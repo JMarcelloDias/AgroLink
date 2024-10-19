d9d0702b114eba897f3b94c4789007ec073af866
João Marcelo Dias <132114906+JMarcelloDias@users.noreply.github.com>
2024-10-19 17:00:39 -0300

    Caminho do cliente-comprador completo

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function showMenu(){
+    var mobileMenu = document.getElementById('menu_mobile');
+    var header = document.querySelector('header');
+
+    if(mobileMenu.style.display == 'flex'){
+        mobileMenu.style.display = 'none';
+        mobileMenu.style.boxShadow = 'none';
+        header.style.boxShadow = '0px 4px 18px rgb(203, 203, 203)';
+    }else{
+        mobileMenu.style.display = 'flex';
+        header.style.boxShadow = 'none';
+    }
+}
+
+window.addEventListener('resize', function() {
+    var mobileMenu = document.getElementById('menu_mobile'); 
+    if (window.innerWidth > 450) {
+        mobileMenu.style.display = 'none';
+    }
+});
+
+function AbrirFiltro(valor){
+    if(valor === 1 ){
+        document.getElementById('filtrar').style.display = 'flex';
+    }else if(valor === 0){
+        document.getElementById('filtrar').style.display = 'none';
+    }
+}
